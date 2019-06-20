7a5cbd54a405c5a1f2a84db3c1c36ae60733fe72
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-20 21:07:13 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(name) # Setter method
+    @name = name
+  end
+
+  def name # Getter/putter method
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+end
